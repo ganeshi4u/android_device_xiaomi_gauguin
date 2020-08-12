9253d743c7c91e02e7c83de9769e1ac61ee60155
LuK1337 <priv.luk@gmail.com>
2020-08-12 11:29:08 +0000

    gauguin: init: Add 6, 8 & 12GB RAM support
    Change-Id: I466f1eccc964204dd51f1c044346ff2c707d6c37

--- libinit/init_gauguin.cpp
+++ libinit/init_gauguin.cpp
@@ -37,6 +37,7 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
@@ -61,6 +62,28 @@ void property_override(char const prop[], char const value[], bool add = true) {
         __system_property_update(pi, value, strlen(value));
     else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void load_dalvik_properties() {
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+    if (sys.totalram < 6144ull * 1024 * 1024) {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("dalvik.vm.heapgrowthlimit", "256m");
+        property_override("dalvik.vm.heapsize", "512m");
+        property_override("dalvik.vm.heapmaxfree", "32m");
+    } else {
+        // 8GB & 12GB RAM
+        property_override("dalvik.vm.heapstartsize", "32m");
+        property_override("dalvik.vm.heapgrowthlimit", "512m");
+        property_override("dalvik.vm.heapsize", "768m");
+        property_override("dalvik.vm.heapmaxfree", "64m");
+    }
+
+    property_override("dalvik.vm.heaptargetutilization", "0.5");
+    property_override("dalvik.vm.heapminfree", "8m");
 }
 
 void set_device_props(const std::string fingerprint, const std::string description,
@@ -108,6 +131,8 @@ void vendor_load_properties() {
             fp_desc,
             "Xiaomi", "gauguininpro", "M2007J17I");
 
+    load_dalvik_properties();
+    
     //Safetynet workarounds
     property_override("ro.oem_unlock_supported", "0");
     property_override("ro.boot.verifiedbootstate", "green");
