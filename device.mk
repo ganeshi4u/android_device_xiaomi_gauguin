6c3981581c1a633e846b0e80ab4a92984a074ac1
TH779 <tongsj2333@gmail.com>
2020-12-10 08:42:32 +0800

    gauguin: Import power HAL from LA.UM.8.12.r1-14400-sm8250.0
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: Idadfea60080ef6917dec0ae93ca82b3c6320eafa
    Reviewed-on: https://mokeedev.review/c/MoKee/android_device_xiaomi_lmi/+/49761

--- device.mk
+++ device.mk
@@ -98,7 +98,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power@1.2-service.gauguin
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
