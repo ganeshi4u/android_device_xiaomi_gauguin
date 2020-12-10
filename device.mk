820caaaa63e73dfefed7e7a7abd215c2d235c412
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-12-11 00:04:42 +0100

    gauguin: Add verified boot permission

--- device.mk
+++ device.mk
@@ -38,4 +38,5 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml
