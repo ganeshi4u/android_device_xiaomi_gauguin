66f63bccdf5d0d654022a340eb1cb375eb600a66
ganeshi4u <ganeshvarmai4u@gmail.com>
2021-01-29 01:55:13 +0530

    gauguin: Copy over the fstab to ramdisk

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_res_images
 
+# Fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
