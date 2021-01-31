4d817b17aa8a3561ec7e1054922c46bdd3fdc72a
ganeshi4u <ganeshvarmai4u@gmail.com>
2021-01-31 17:20:44 +0530

    gauguin: Convert prop files to make

---
+++ product_prop.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Audio
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=7
+
+# Bluetooth
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.bluetooth.library_name=libbluetooth.so
